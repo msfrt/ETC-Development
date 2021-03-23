64ed89fa8c4261cde657c7d230cbda40cb60fb2e
kopecnic <kopecnic@msu.edu>
2021-03-22 20:52:52 -0400

    start motor modeling

---
+++ matlab_code/etb_model/DC_Motor_Parameter_Initialization.m
@@ -0,0 +1,9 @@
+% DC Motor Parameter Initialization
+clear all
+
+J  = 0.0021; % kg.m^2
+b  = 0.0088;  % N.m.s
+Ke = 0.383; % V/rad/sec
+Kt = 0.383; % N.m/amp
+R  = 1.215;    % Ohm
+L  = 0.00106;  % H
