0d4bdbdf29bcce36e510b788c1e8d1b720f7f5df
flipper92017 <59802734+flipper92017@users.noreply.github.com>
2021-04-07 16:03:55 -0400

    More Processing of data

--- matlab_code/etb_model/pulseRange.m
+++ matlab_code/etb_model/pulseRange.m
@@ -1,5 +1,7 @@
 close all
-inputs = matfile('myFile.mat')
+load('Recorder_2021-04-01_23-18-08')
+
+%% Get Ranges
 index=1;
 for i= 2:length(ETC_motorAPWM_t2)
        if (ETC_motorAPWM_t2(i)- ETC_motorAPWM_t2(i-1) > 0)
@@ -10,7 +12,21 @@ for i= 2:length(ETC_motorAPWM_t2)
            index = index + 1;
        end
 end
-
+%% 
+timeLengths =0
+table =0;
+for i = (1:length(pulseCord))
+    timeLengths(i) = length(pulseCord(i,1):pulseCord(i,2))
+    for j = (1:max(timeLengths))
+        table(j,1) = t3(j)-t3(1);
+    end
+    for j = (1:pulseCord(i,2)-pulseCord(i,1))
+        table(j,i+1) = ETC_throttlePosition1_pct_t3(pulseCord(i,1)+j);
+    end
+end
+signalTimeSeries = timeseries(table(:,2:size(table,2)),table(:,1))
+plot(signalTimeSeries)
+%%
 figure(1);
 hold on;
 for i = 1:length(pulseCord)
@@ -18,3 +34,4 @@ for i = 1:length(pulseCord)
 end
 legend(string(pulseCord(:,3)));
 hold off;
+save('pulseCord.mat','pulseCord');
